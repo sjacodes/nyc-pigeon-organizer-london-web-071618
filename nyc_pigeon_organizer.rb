7fb765b366ce9c4323a55299dd556f2fea48996e
Sarah Jacob <smjacob91@gmail.com>
2018-07-12 11:17:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+    data.each do |category, info|
+      info.each do |characteristic, names_array|
+        names_array.each do |name|
+          new_hash["#{name}"] = {}
+        end
+      end
+    end
+    data.each do |category, info|
+      info.each do |characteristic, names_array|
+        names_array.each do |name|
+          new_hash["#{name}"][:"#{category}"] = []
+        end
+      end
+    end
+    data.each do |category, info|
+      info.each do |characteristic, names_array|
+        data[category][characteristic].each do |name|
+          new_hash[name][category].push(characteristic.to_s)
+        end
+      end
+    end
+  return new_hash
 end
+      

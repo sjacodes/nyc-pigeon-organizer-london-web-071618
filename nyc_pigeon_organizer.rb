78b6dacc4e3e10e60baaaf96633e768d63cd31a4
Sarah Jacob <smjacob91@gmail.com>
2018-07-17 16:38:11 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,16 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-    data.each do |category, info|
-      info.each do |characteristic, names_array|
+    data.each do |category, characteristics|
+      characteristics.each do |characteristic, names_array|
         names_array.each do |name|
-          new_hash["#{name}"] = {}
+          new_hash[name] = {}
         end
       end
     end
     data.each do |category, info|
       info.each do |characteristic, names_array|
         names_array.each do |name|
-          new_hash["#{name}"][:"#{category}"] = []
+          new_hash[name][:"#{category}"] = []
         end
       end
     end
